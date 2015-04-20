5aa7fe6221ba8c530703588596dddf83ab45c302
Trey Griffith <trey.griffith@gmail.com>
2015-04-19 19:22:30 -0700

    add cors support

--- middleware.js
+++ middleware.js
@@ -1,6 +1,8 @@
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'),
+    cors = require('cors')
 
 function middleware(app) {
+  app.use(cors());
   app.use(bodyParser.raw({
     type: '*/*',
     limit: '1mb'
