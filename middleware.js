d60e46546bf70988259fef4845176e48174cb4e8
Trey Griffith <trey.griffith@gmail.com>
2015-04-20 18:03:29 -0700

    get rid of old code

--- middleware.js
+++ middleware.js
@@ -1,19 +1,5 @@
 var bodyParser = require('body-parser'),
-    cors = require('cors'),
-    rate = require('express-rate'),
-    redis = require('redis'),
-    url = require('url'),
-    redisClient;
-
-if(process.env.REDISCLOUD_URL) {
-  var redisUrl = url.parse(process.env.REDISCLOUD_URL);
-  redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-  redisClient.auth(redisURL.auth.split(":")[1]);
-} else {
-  redisClient =redis.createClient();
-}
-
-var redisHandler = new rate.Redis.RedisRateHandler({ client: redisClient });
+    cors = require('cors');
 
 function middleware(app) {
   app.use(cors(app.settings.cors));
